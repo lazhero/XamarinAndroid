9c084585f734fa100dcf6b4f082e6b81cc86a25b
Gabriel Solano Coronado <gabrielsolcor@estudiantec.cr>
2020-07-09 23:15:49 -0600

    [COOK-66] Add simple interface

---
+++ CookTime/CookTime/SingInActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace CookTime
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class SingInActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resourceev
+            SetContentView(Resource.Layout.SingInLayout);
+
+            Button buttonSingIn = FindViewById<Button>(Resource.Id.singinButton);
+            Button buttonSingUp = FindViewById<Button>(Resource.Id.singupButton);
+            buttonSingIn.Click += ButtonSingIn_Click;
+            buttonSingUp.Click += ButtonSingUp_Click;
+        }
+
+        private void ButtonSingUp_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(SingUpActivity));
+            StartActivity(intent);
+        }
+
+        private void ButtonSingIn_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(newFeed));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ CookTime/CookTime/SingUpActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CookTime
+{
+    [Activity(Label = "SingUnActivity")]
+    public class SingUpActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SingUpLayout);
+            Button buttonSingIn = FindViewById<Button>(Resource.Id.buttonSingIn2);
+            buttonSingIn.Click += ButtonSingIn_Click;
+
+          
+           
+
+
+            // Create your application here
+        }
+
+        private void ButtonSingIn_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(SingInActivity));
+            StartActivity(intent);
+        }
+    }
+}
